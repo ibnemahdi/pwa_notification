ab36c333daaabbdc5aeaae6f614cfa8a8d79faab
ibnemahdi <ibnemahdi@gmail.com>
2023-04-07 18:53:44 -0500

    buttons

--- src/serviceworker.js
+++ src/serviceworker.js
@@ -38,32 +38,15 @@ event.respondWith(
 
  
 self.addEventListener('notificationclick', (event) => {
-    
-    
-    /*
-    if (!event.action) {
-        // Was a normal notification click
-        console.log('Notification Click.');
-        return;
-    }
-
-    const clickedNotification = event.notification;
-    console.log(clickedNotification);
-    
-    clickedNotification.close();
-  
-    // Do something as the result of the notification click
-    const promiseChain = clients.openWindow('https://www.google.com');
-    event.waitUntil(promiseChain);*/
     const rootUrl = new URL('./', location).href; 
     event.notification.close();
     const click_event_msg = {   
         actions:event.notification.actions, 
         data:event.notification.data,
-        icon:event.notification.icon,
-        badge:event.notification.badge,
+        icon:'/favicon.ico',
+        badge:'images/badge.png',
         title:event.notification.title,
-        body:event.notification.title,
+        body:event.notification.body,
         clicked_action:event.action
     };
     const rootUrlWithParameters = `${rootUrl}?msg_payload=${JSON.stringify(click_event_msg)}`;
@@ -76,16 +59,12 @@ self.addEventListener('notificationclick', (event) => {
                 if (client.url.indexOf(rootUrl) >= 0)
                 {
                     client.focus();
-                   
-                    console.log("Service Worker data:",click_event_msg )
                     client.postMessage(click_event_msg);
                     return;
                 }
             }
-
             return clients.openWindow(rootUrlWithParameters).then(function (client) { 
-                            //client.focus() 
-                            //client.postMessage("hello World");
+                            client.focus() 
                         
                         });
         })
@@ -93,37 +72,6 @@ self.addEventListener('notificationclick', (event) => {
   });
 
 
-
-
-/*
-  self.addEventListener('push', async function(event) {
-
-    const data = event.data.json();
-
-    event.waitUntil(
-        self.registration.showNotification(data.notification.title, 
-        {
-            body: data.notification.body,
-            icon:data.notification.icon,
-            image:data.notification.image,
-            badge:'images/badge.png',
-            actions: data.notification.actions,
-        })
-    );
-
-    // Check if any clients are currently open and in the foreground
-    const clientsList = await self.clients.matchAll({ type: 'window' });
-    const client = clientsList.find(c => c.visibilityState === 'visible');
-    if (client) {
-        // Application is in the foreground, do something
-        console.log('Application is in the foreground');
-    } else {
-        // Application is in the background, do something else
-        console.log('Application is in the background');
-    }
-});
-
-*/
 self.addEventListener('push', (e) => {
     const data = e.data.json();
     e.waitUntil(
@@ -133,11 +81,12 @@ self.addEventListener('push', (e) => {
         console.log('app is in foreground')
         const click_event_msg = {   
             body: data.notification.body,
-            icon:data.notification.icon,
+            icon:'/favicon.ico',
             image:data.notification.image,
             badge:'images/badge.png',
             actions: data.notification.actions,
-            title:data.notification.title
+            title:data.notification.title,
+            data:event.notification.data,
         };
         console.log("Service Worker data:",click_event_msg )
         client.postMessage(click_event_msg);
@@ -151,11 +100,12 @@ self.addEventListener('push', (e) => {
             self.registration.showNotification(data.notification.title, 
             {
                 body: data.notification.body,
-                icon:data.notification.icon,
+                icon:'/favicon.ico',
                 image:data.notification.image,
                 badge:'images/badge.png',
                 actions: data.notification.actions,
-                title:data.notification.title
+                title:data.notification.title,
+                data:event.notification.data,
             })
         );
         }
